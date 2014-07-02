722e5a2bee6fe7c1cfaf36e4dbb2dd0f55a6d7e3
sherpper <sheweize@gmail.com>
2014-07-03 07:09:44 +0800

    project file change

---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh --login -i
+cd /f/all-tech/source_code/GitDns
+git add hosts
+git commit -m 'ip have been changed'
+git push origin dev
---
+++ ip_server.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set URL="http://iframe.ip138.com/ic.asp"
+
+:again
+
+for /f "tokens=2 delims=[]" %%a in ('wget -q "%URL%" -O -') do (
+
+    set "PublicIP=%%a"
+
+)
+
+:: 等于null 要严格测试
+if defined PublicIP (
+ping 127.1 -n 10>nul
+goto again
+)
+
+for /f "tokens=1" %%i in (hosts)
+
+if %PublicIP%==%%i (
+exit
+)else (
+echo %PublicIP% jamka.ze > F:\all-tech\source_code\GitDns\hosts
+sh commit.sh
+exit
+)
+
+pause
