575d112cd6b81ef3799e728d44d76a734e82fc4a
sherpper <sheweize@gmail.com>
2014-07-17 22:55:56 +0800

    debug_4

--- commit.sh
+++ commit.sh
@@ -1,5 +1,7 @@
 cd /d/dns/GitDns
 echo $PATH
+x=`date today '+%Y-%m-%e %T'`
 git add hosts
-git commit -m 'ip have been changed'
+git commit -m 'ip have been changed_`echo $x`'
 git push origin dev
+exit
