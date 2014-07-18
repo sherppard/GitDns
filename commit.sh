2c790a2e0edf414b27d6f72a7dc8608c175c69d7
sherpper <sheweize@gmail.com>
2014-07-18 15:55:27 +0800

    debug sessuse

--- commit.sh
+++ commit.sh
@@ -5,6 +5,6 @@ echo $PATH
 export x=`date -d today '+%Y-%m-%e %T'`
 git add hosts
 git commit -m "ip have been changed_`echo $x`"
-ping -n 5 127.0.0.1 > nul
+ping -n 5 127.0.0.1 > null
 git push origin dev
 exit
