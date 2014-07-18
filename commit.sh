0e5746ea933e650ce5a0aeb3d1890378146728bb
sherpper <sheweize@gmail.com>
2014-07-18 15:14:20 +0800

    xxxxxxxxx

--- commit.sh
+++ commit.sh
@@ -5,6 +5,6 @@ echo $PATH
 export x=`date -d today '+%Y-%m-%e %T'`
 git add hosts
 git commit -m "ip have been changed_`echo $x`"
-git log -pretty=oneline
+git log --pretty=oneline
 git push origin dev
 exit
