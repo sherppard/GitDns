c18e655d37918f8c4c0f13027e468ad4ba37ca4f
sherpper <sheweize@gmail.com>
2014-07-18 15:10:07 +0800

    xxxx

--- commit.sh
+++ commit.sh
@@ -2,8 +2,9 @@ cd /d/dns/GitDns
 pwd
 PATH=/c/Users/Administrator/bin:.:/usr/local/bin:/mingw/bin:/bin:$PATH
 echo $PATH
-x=`date -d today '+%Y-%m-%e %T'`
+export x=`date -d today '+%Y-%m-%e %T'`
 git add hosts
 git commit -m "ip have been changed_`echo $x`"
+git log -pretty=oneline
 git push origin dev
 exit
