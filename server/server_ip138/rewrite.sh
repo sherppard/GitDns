26b3f3eedc4bbf9913515f4b5d2e631d13967e54
sherpper <sheweize@gmail.com>
2014-07-22 12:03:31 +0800

    debug_6

--- server/server_ip138/rewrite.sh
+++ server/server_ip138/rewrite.sh
@@ -1,5 +1,5 @@
-# 提取出当前的ip地址
 PublicIP=`wget -q "http://20140507.ip138.com/ic.asp" -O - | grep '[.*?]' | cut -d[ -f2 | cut -d] -f1`
+# 提取出当前的ip地址
 
 # 重新提取ip地址
 refetchIP(){
@@ -9,7 +9,7 @@ refetchIP(){
 
 # 判断wget是否提取到ip地址
 checkIP(){
-	if [ -z "$PublicIP" ]; then
+	if [ -z $PublicIP ]; then
 		refetchIP
 	fi
 }
@@ -19,6 +19,7 @@ checkIP
 # 提取出hosts文件的ip地址
 beforeIP=`cat ../../hosts | grep sherpper | cut -d' ' -f1`
 
+# 用read进行sh脚本的调试 
 # 在hosts文件下,进行ip地址替换操作
-sed -i "/sherpper/s/`echo $beforeIP`/`echo $PublicIP`/" hosts  # notice: the double quotes
+sed -i "/sherpper/s/`echo $beforeIP`/`echo $PublicIP`/" ../../hosts  # notice: the double quotes
 exit
