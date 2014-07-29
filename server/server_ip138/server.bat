3499b36adad9dea1810519077291845d4f50b44f
sherpper <sheweize@gmail.com>
2014-07-29 13:47:31 +0800

    test finish

--- server/server_ip138/server.bat
+++ server/server_ip138/server.bat
@@ -32,7 +32,6 @@ D:\"Program Files (x86)"\Git\bin\sh.exe rewrite.sh
 :: 运行提交到github的脚本,notice: 修改sh.exe 的路径
 cd %HOMEDRIVE%%HOMEPATH%  
 D:\"Program Files (x86)"\Git\bin\sh.exe --login -i commit.sh
-pause
 exit
 )
 
